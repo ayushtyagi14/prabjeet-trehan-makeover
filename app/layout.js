a1c518caf633109fa14dfd57d740126afb5fef9d
Ayush Tyagi <ayush.tyagi145@gmail.com>
2024-10-22 20:04:20 +0530

    added google ads tag

--- app/layout.js
+++ app/layout.js
@@ -1,23 +1,40 @@
-import { DM_Sans } from 'next/font/google'
-import { Josefin_Sans } from 'next/font/google'
-import './globals.css'
+import { DM_Sans } from 'next/font/google';
+import { Josefin_Sans } from 'next/font/google';
+import './globals.css';
+import Head from 'next/head';
+import { useEffect } from 'react';
+import localFont from 'next/font/local';
 
-import localFont from 'next/font/local'
+export const amsterdam = localFont({ src: '../public/amsterdamFour.ttf' });
+export const stone = localFont({ src: '../public/stonecrusher.ttf' });
 
-export const amsterdam = localFont({ src: '../public/amsterdamFour.ttf' })
-export const stone = localFont({ src: '../public/stonecrusher.ttf' })
-
-const dmSans = DM_Sans({ subsets: ['latin'] })
-export const relieve = Josefin_Sans({ subsets: ['latin'] })
+const dmSans = DM_Sans({ subsets: ['latin'] });
+export const relieve = Josefin_Sans({ subsets: ['latin'] });
 
 export default function RootLayout({ children }) {
+  useEffect(() => {
+    // Initialize the Google Tag Manager
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      window.dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', 'G-587P6VWZPW');
+  }, []);
+
   return (
     <html lang="en">
-      <head>
+      <Head>
         <title>Prabjeet Trehan Makeover</title>
-        <meta name='description' content='Description' />
-      </head>
+        <meta name="description" content="Description" />
+        
+        {/* Google Tag Manager Script */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-587P6VWZPW"
+        ></script>
+      </Head>
       <body className={dmSans.className}>{children}</body>
     </html>
-  )
+  );
 }

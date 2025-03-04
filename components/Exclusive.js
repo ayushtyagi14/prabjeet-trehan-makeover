5583c0b31830aabf98beb41926b0b4017a79773b
Ayush Tyagi <ayush.tyagi145@gmail.com>
2025-03-04 23:05:04 +0530

    storefront link added

--- components/Exclusive.js
+++ components/Exclusive.js
@@ -10,18 +10,18 @@ const Exclusive = () => {
             whileInView={{ opacity: 1 }}
             transition={{ duration: 1 }}
             className="max-w-screen-2xl mx-auto py-12 px-4"
-            id="shop"
+            id="amazon"
         >
             <h1 className={`${stone.className} text-[60px] md:text-[90px] text-gray-800 text-center`}>
-                Exclusive Picks
+                Click To See Exclusive Products
             </h1>
             <div className="flex flex-col w-[65%] mx-auto gap-4">
-                <h2 className='text-center text-[24px] font-light -mt-6 tracking-wide'>
-                    Shop the products I love and recommend for the perfect makeover
+                <h2 className='text-center md:text-[24px] text-[20px] font-light -mt-6 tracking-wide'>
+                    Click below to explore my handpicked Amazon storefront recommendations!
                 </h2>
                 <div className='mx-auto'>
                     <Link
-                        href={'https://www.amazon.com/shop/prabjeettrehanmakeover?ref_=cm_sw_r_cp_ud_aipsfshop_MTJ9W1AA5NMPHR6AEBJA'}
+                        href={'https://www.amazon.com/shop/prabjeettrehanmakeover?ref_=cm_sw_r_cp_ud_aipsfshop_15X8QAFE3SYYQKQCWRJE'}
                         target='_blank'
                     >
                         <img src="/amazon-big.png" alt="amazon" className='border-[2px] border-[#2b4dac] rounded-[24px] p-4 shadow-md hover:shadow-xl cursor-pointer' />
